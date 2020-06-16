3167e9a8f31dc34978450d717b9744a4e48848d8
jiajunshuai <438249938@qq.com>
2020-06-16 11:23:43 +0800

    商品

---
+++ app/Http/Controllers/Goods/GoodsController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Goods;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Model\GoodsModel;
+class GoodsController extends Controller
+{
+    public function detail(){
+        $goods_id=$_GET['id'];
+        //echo $goods_id;
+        //$info=GoodsModel::where(['goods_id'=>$goods_id])->get()->toArray();
+        $info=GoodsModel::find($goods_id);
+        print_r($info);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/test/hello','TestController@hello');
+
+Route::get('/goods/detail','Goods\GoodsController@detail');//商品详情
