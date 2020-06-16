d4d97ffc1fbae7940d070a2156d23aa26b158b88
jiajunshuai <438249938@qq.com>
2020-06-17 06:20:12 +0800

    h

--- app/Http/Controllers/Goods/GoodsController.php
+++ app/Http/Controllers/Goods/GoodsController.php
@@ -10,8 +10,9 @@ class GoodsController extends Controller
     public function detail(){
         $goods_id=$_GET['id'];
         //echo $goods_id;
-        //$info=GoodsModel::where(['goods_id'=>$goods_id])->get()->toArray();
-        $info=GoodsModel::find($goods_id);
+        //$info=GoodsModel::where(['goods_id'=>$goods_id])->get()->toArray();   //二维数组
+        //$info=GoodsModel::find($goods_id);
+        $info=GoodsModel::where(['goods_id'=>$goods_id])->first()->toArray();   //一维数组
         print_r($info);
     }
 }
