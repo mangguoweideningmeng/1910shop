dc11cfdf5cdcfd5aea41176761132d6be7be5032
jiajunshuai <438249938@qq.com>
2020-06-20 16:04:05 +0800

    -a

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Redis;
 class TestController extends Controller
 {
     public function hello(){
         //echo __METHOD__;
         echo 11;
+    }
+    public function redis1(){
+        $key='name2';
+        $val1=Redis::get($key);
+        var_dump($val1);echo '</br>';
+        echo '$val1'.$val1;
     }
 }
